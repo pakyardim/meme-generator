793c0d4abce95b4ca1682c72a30b9829a20ca08f
U200709050 <emirhanpakyardim@gmail.com>
2022-08-08 13:33:07 +0300

    init commit

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+const Header = () => {
+
+    return(
+        <header>
+            <h2>Meme Generator</h2>
+            <h3>By Emirhan baby woho</h3>
+        </header>
+    )
+}
+
+
+export default Header;
